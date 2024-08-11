7a80f7694a180644abd676654815151a58b0fe74
Bryant Baird <bryantabaird@gmail.com>
2024-08-10 22:14:37 -0600

    feat(pz-9): tenant DB on LocalStack (#10)
    fix #9
    
    Co-authored-by: Bryant Baird <--local>

---
+++ apps/web/app/api/tenant/route.ts
@@ -0,0 +1,60 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { randomUUID } from "crypto";
+
+import { TENANT_TABLE_NAME } from "@repo/shared";
+import { tenantSchema } from "./type";
+
+const client = new DynamoDBClient({
+  endpoint: "http://localhost:4566",
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: "test",
+    secretAccessKey: "test",
+  },
+});
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+export async function POST(req: Request) {
+  const { organizationName } = await req.json();
+
+  const id = randomUUID();
+  const dateCreated = new Date().toISOString();
+
+  const tenant = { organizationName, id, dateCreated };
+
+  console.log("Adding tenant", tenant);
+
+  const { success, error } = tenantSchema.safeParse(tenant);
+
+  if (!success) {
+    const errorMessage = "Tenant schema is invalid";
+    console.error(errorMessage, error);
+    return Response.json({ error: errorMessage }, { status: 400 });
+  }
+
+  try {
+    await documentClient.send(
+      new PutCommand({
+        TableName: TENANT_TABLE_NAME,
+        Item: tenant,
+      }),
+    );
+  } catch (error) {
+    console.error("Error adding tenant", error);
+    return Response.json({ error: "Failed to add tenant" }, { status: 500 });
+  }
+
+  return Response.json({ data: tenant });
+}
+
+export async function PUT() {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  return Response.json({ message: "PUT: Hello, world!" });
+}
+
+export async function DELETE() {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  return Response.json({ message: "DELETE: Hello, world!" });
+}
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+import { TENANT_TABLE_NAME } from "@repo/shared";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define an S3 bucket
+    new s3.Bucket(this, "MyBucket", {
+      bucketName: "my-cdk-bucket",
+      versioned: true, // Enable versioning for the bucket
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a DynamoDB table for tenants
+    new dynamodb.Table(this, "Tenant", {
+      tableName: TENANT_TABLE_NAME,
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
   }
 }
