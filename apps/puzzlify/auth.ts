8111c0c89eb2c8e05304f8645cbdd9c18f647d43
Bryant Baird <--local>
2024-08-16 13:19:59 -0600

    Debug login

--- apps/puzzlify/auth.ts
+++ apps/puzzlify/auth.ts
@@ -19,28 +19,40 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           return null;
         }
 
-        if (!credentials.email || !credentials.password) {
-          return null;
-        }
+        try {
+          if (!credentials.email || !credentials.password) {
+            return null;
+          }
 
-        const email = credentials.email as string;
-        const password = credentials.password as string;
+          const email = credentials.email as string;
+          const password = credentials.password as string;
 
-        const user = await getItem(email, AUTH_USER_TABLE_NAME);
+          const user = await getItem(email, AUTH_USER_TABLE_NAME);
 
-        if (!user) {
-          return null;
-        }
+          console.log("getting user", user);
 
-        const isPasswordMatch = await bcrypt.compare(
-          password,
-          user.hashedPassword,
-        );
+          console.log("email", email);
+          console.log("password", password);
 
-        if (isPasswordMatch) {
-          return { id: user.id, email: user.email };
-        } else {
-          throw new Error("Invalid email or password");
+          if (!user) {
+            return null;
+          }
+
+          const isPasswordMatch = await bcrypt.compare(
+            password,
+            user.hashedPassword,
+          );
+
+          console.log("isPasswordMatch", isPasswordMatch);
+
+          if (isPasswordMatch) {
+            return { id: user.id, email: user.email };
+          } else {
+            throw new Error("Invalid email or password");
+          }
+        } catch (error) {
+          console.error("Error signing in", error);
+          throw new Error("Failed to sign in");
         }
       },
     }),
