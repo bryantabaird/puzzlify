15e0da0f55c6bdb8586ae6ef0919d96115ace9ba
Bryant Baird <--local>
2024-09-04 21:12:04 -0600

    Fixing aws sdk package

--- apps/puzzlify/next.config.mjs
+++ apps/puzzlify/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // next build would fail, suggestion here inspired by: https://github.com/aws/aws-sdk-js-v3/issues/6411
+    serverComponentsExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/s3-request-presigner']
+  }
+};
 
 export default nextConfig;
